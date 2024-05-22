dc0c501c86305dd7530e08af5aa088d0aa8be5ea
subikstha <subsatch@gmail.com>
2024-05-22 21:43:56 +0545

    Font configured

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,22 @@
 import type { Config } from 'tailwindcss';
+import { colors, white, black, transparent, currentColor } from './src/tokens/colors';
 
 export default {
   content: ['./src/**/*.tsx', './src/**/*.ts', './src/**/*.mdx'],
   darkMode: ['class', '[data-mode="dark"]'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter Variable', 'sans-serif'],
+      },
+    },
+    colors: {
+      ...colors,
+      white,
+      black,
+      transparent,
+      currentColor,
+    },
+  },
   plugins: [],
 } satisfies Config;
